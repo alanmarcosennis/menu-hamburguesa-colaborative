a6970812f10c088d7148ae2e9236605e3a4d0206
alanmarcosennis <alanmarcosennis17@gmail.com>
2023-05-26 15:36:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+  
+    menuToggle.addEventListener('click', function() {
+      menu.classList.toggle('show');
+    });
+  });
+  
